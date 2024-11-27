665adc4e2c4f54cb628abc49d0b28afe497de51e
Divanshu9090 <divanshuagrawal9090@gmail.com>
2024-11-27 11:16:46 +0530

    Remove unwanted files

--- recipe_frontend/src/App.jsx
+++ recipe_frontend/src/App.jsx
@@ -2,10 +2,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Auth from './components/Auth.jsx';
 import Home from './components/MainScreen.jsx';
-import Category from './components/category.jsx';
 import SignupDetail from './components/SignupDetail.jsx';
-import UploadPage from './components/UploadPage.jsx'; // Import UploadPage Component
-import Profile from './components/profilePage.jsx';
 
 // Define routes for your app
 const router = createBrowserRouter([
@@ -18,27 +15,14 @@ const router = createBrowserRouter([
     element: <Home />,  // Route for home page
   },
   {
-    path: '/categories',
-    element: <Category />,  // Route for category page
-  },
-  {
-    path: '/upload/:category', // Route for upload page with category parameter
-    element: <UploadPage />,
-  },
-  {
     path: '/Signupdetails', 
     element: <SignupDetail />,
-  },
-  {
-    path: '/profile',
-    element: <Profile />, 
   },
 ]);
 
 function App() {
   return (
     <>
-      {/* Use RouterProvider to provide the router to the application */}
       <RouterProvider router={router} />
     </>
   );
